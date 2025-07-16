615eaec7f0173799305acd51e96c76c8f42ec556
Michelle Bollino <jackandzoe.rosen@gmail.com>
2025-07-15 21:02:45 -0400

    Added about css, added index page with css, added product model and created database, created superuser for admin

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('products/', views.product_index, name='product_index'),
    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product
 
 def home(request):
-    return HttpResponse('<h1>Hello</h1>')
+    return render(request, 'home.html')
     
 def about(request):
     return render(request, 'about.html')
+
+def product_index(request):
+    products = Product.objects.all()
+    return render(request, 'products/index.html', {'products': products})
     
 
