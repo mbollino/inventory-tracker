2b5ff16698cf65d27b5604c7da07eef4f382d0bf
Michelle Bollino <jackandzoe.rosen@gmail.com>
2025-07-16 21:00:11 -0400

    created Order model, migrated model, and registered model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=20)
@@ -13,3 +15,12 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         return reverse('product_detail', kwargs={'product_id': self.id})
+    
+class Order(models.Model):
+    quantity_ordered = models.IntegerField()
+    order_date = models.DateField()
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Ordered {self.quantity_ordered()} on {self.order_date}"
