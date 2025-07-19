cf5bbcb662bb5628aa23944a32035993ce8db3cf
Michelle Bollino <jackandzoe.rosen@gmail.com>
2025-07-19 18:14:55 -0400

    Add Address model, migrate text address info from suppliers already added into new address model, remove old address fields, udpate html to render new information, update css to look prettier

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Order, Supplier
+from .models import Product, Order, Supplier, Address
 
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Supplier)
+admin.site.register(Address)
--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.urls import reverse
 
+class Address(models.Model):
+    street_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state=models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+
 class Supplier(models.Model):
     company_name = models.CharField(max_length=100)
     order_link=models.URLField(max_length=200)
     contact_person = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     phone_number = models.CharField(max_length=20)
-    address = models.TextField(max_length=100)
+    address_fk = models.ForeignKey(Address, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.company_name
